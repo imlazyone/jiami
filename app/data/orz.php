d2f760a70464f654562db1a7880d362fd7fbc6d2
Stuart W. <imlazyone@gmail.com>
2014-02-14 22:02:12 +0800

    Tiny update on orz.php

--- app/data/orz.php
+++ app/data/orz.php
@@ -9,16 +9,14 @@ $action = isset ( $_REQUEST['action'] ) ? $_REQUEST['action'] : '';
 $json = isset ( $_REQUEST['json'] ) ? $_REQUEST['json'] : '';
 
 $cur_file = date( 'Y-m-d' ) . '.txt';
-if ( file_exists($cur_file))
+if ( file_exists( $cur_file ) )
 	$cur_json = $_GET['callback'] . '(' . file_get_contents( $cur_file ) . ')'; 
 else
 	$cur_json = $_GET['callback'] . '({"rows":[]})';
 
-if ( $action == 'get' ) {
+if ( 'get' == $action ) {
 	echo $cur_json;
-} else if ( $action == 'set') {
+} else if ( 'set' == $action ) {
 	file_put_contents( $cur_file, $json );
 	echo $_GET['callback'] . '({"success":"1"})';
 }
-
-?>
